83908bde3d86b552dce92a3bf36304dae9a62cc0
robzor92 <robin.eric.andersson@gmail.com>
2017-08-24 14:26:03 +0200

    Add get_hdfs method

--- hopsutil/hopsutil.py
+++ hopsutil/hopsutil.py
@@ -3,3 +3,9 @@ Utility functions to retrieve information about available services and setting u
 
 These utils facilitates development by hiding complexity for programs interacting with Hops services.
 """
+
+import pydoop.hdfs as hdfs
+import os
+
+def get_hdfs():
+    return hdfs.open('default', 0, os.getenv("USER"))
