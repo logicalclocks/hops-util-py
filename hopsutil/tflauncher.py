2ac852587fc1ccf5695688183d2ca53a70cdf9f3
robzor92 <robin.eric.andersson@gmail.com>
2017-09-11 18:55:56 +0200

    Initial support for two-cases either one executor, or multiple specified using args_dict

--- hopsutil/tflauncher.py
+++ hopsutil/tflauncher.py
@@ -3,48 +3,49 @@ Utility functions to retrieve information about available services and setting u
 
 These utils facilitates development by hiding complexity for programs interacting with Hops services.
 """
+
 import os
 from hopsutil import hdfs
 from hopsutil import tensorboard
 
-hyperparams = None
+args_dict = None
 
 def launch(sc, map_fun):
 
-    if hyperparams != None:
-        num_executors = hyperparams.values()[0].len()
+    if args_dict == None:
+        num_executors = 1
+    else:
+        num_executors = args_dict.values()[0].len()
 
     #TF task should be run on 1 executor
     nodeRDD = sc.parallelize(range(num_executors), num_executors)
 
     #Force execution on executor, since GPU is located on executor
-    nodeRDD.foreachPartition(prepare_func(map_fun))
+    nodeRDD.foreachPartition(prepare_func(map_fun, args_dict))
 
-def set_hyperparams(grid):
-    global hyperparams
-    hyperparams = grid
+def set_args(args):
+    global args_dict
+    args_dict = args
 
 
 #Helper to put Spark required parameter iter in function signature
-def prepare_func(map_fun):
-    #TODO: Consume iter and put in list of hyperparams
+def prepare_func(map_fun, args_dict):
+
     def _wrapper_fun(iter):
 
         #Start TensorBoard automatically
         tensorboard.register()
 
-        #TensorFlow script
-        if hyperparams == None:
-            map_fun()
-        else:
+        #Multiple executors
+        if args_dict != None:
             argcount = map_fun.func_code.co_argcount
             names = map_fun.func_code.co_varnames
 
             args = []
             argIndex = 0
             while argcount > 0:
-                #Get hyperparam value for
-                args.append(hyperparams[names[argIndex]][iter])
+                #Get args for executor and run function
+                args.append(args_dict[names[argIndex]][iter])
                 argcount -= 1
                 argIndex += 1
             map_fun(*args)
@@ -52,13 +53,12 @@ def prepare_func(map_fun):
         #Current applicationId
         app_id = os.getenv("APP_ID")
 
-        #Are we allowed to create this?
-
+        #Create output directory for TensorBoard events for this executor
         hdfs_events_parent_dir = hdfs.project_path() + "/Jupyter/Tensorboard"
         dir_exists = hdfs.exists(hdfs_events_parent_dir)
         if dir_exists == False:
             hdfs.create_directory(hdfs_events_parent_dir)
-        hdfs_events_logdir = hdfs_events_parent_dir + "/" + app_id
+        hdfs_events_logdir = hdfs_events_parent_dir + "/" + app_id + ".exec." + iter
         hdfs.create_directory(hdfs_events_logdir)
 
         #Write TensorBoard logdir contents to HDFS
