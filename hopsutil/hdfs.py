884b0c21bcb8bd8af383fb6f7fad2340577bd3af
robzor92 <robin.eric.andersson@gmail.com>
2017-10-02 10:26:06 +0200

    append instead of overwriting

--- hopsutil/hdfs.py
+++ hopsutil/hdfs.py
@@ -11,6 +11,9 @@ import datetime
 def get():
     return hdfs.hdfs('default', 0, user=project_user())
 
+def get_fs():
+    return hdfs.fs.hdfs('default', 0, user=project_user())
+
 def project_path():
     hops_user = os.environ["SPARK_USER"]
     hops_user_split = hops_user.split("__")
@@ -22,13 +25,15 @@ def project_user():
     return hops_user
 
 def log(string):
+    fs_handle = get_fs()
     if isinstance(string, basestring):
         logfile = os.environ['EXEC_LOGFILE']
-        hdfs.dump('{0}: {1}'.format(datetime.datetime.now().isoformat(), string), logfile, user=project_user())
+        with fs_handle.open(logfile) as f:
+            f.write('{0}: {1}'.format(datetime.datetime.now().isoformat(), string) + '\n')
     else:
         logfile = os.environ['EXEC_LOGFILE']
-        hdfs.dump('{0}: {1}'.format(datetime.datetime.now().isoformat(), 'ERROR! Attempting to write a non-basestring object to logfile'),
-                  logfile, user=project_user())
+        with fs_handle.open(logfile) as f:
+            f.write('{0}: {1}'.format(datetime.datetime.now().isoformat(), 'ERROR! Attempting to write a non-basestring object to logfile') + '\n')
 
 def create_directories(app_id, run_id, executor_num):
     pyhdfs_handle = get()
