e67eaf16d01df8b0816ecae56f3f4e16b4a5c328
robzor92 <robin.eric.andersson@gmail.com>
2017-10-17 15:35:43 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def read(fname):
 
 setup(
     name='hops',
-    version='1.1.1',
+    version='1.1.2',
     author='Robin Andersson',
     author_email='robin2@kth.se',
     description='A helper library for Hops that facilitates development by hiding the complexity of discovering services and setting up security.',
     license='Apache License 2.0',
     keywords='HOPS, Hadoop',
     url='https://github.com/hopshadoop/hops-util-py',
-    download_url = 'https://github.com/hopshadoop/hops-util-py/archive/1.1.1.tar.gz',
+    download_url = 'https://github.com/hopshadoop/hops-util-py/archive/1.1.2.tar.gz',
     packages=['hops'],
     long_description=read('README.rst'),
     classifiers=[
