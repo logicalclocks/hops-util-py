5b269b440869ed337c3d81b847bbc0c2ba4a03e8
robzor92 <robin.eric.andersson@gmail.com>
2017-11-06 15:32:40 +0100

    Add support for visualization after training

--- hops/tensorboard.py
+++ hops/tensorboard.py
@@ -18,6 +18,7 @@ exec_logdir = None
 params = None
 dir_counter = 0
 tb_pid = 0
+tb_url = None
 endpoint = None
 
 def register(hdfs_exec_dir, endpoint_dir, exec_num, param_string=None):
@@ -33,7 +34,7 @@ def register(hdfs_exec_dir, endpoint_dir, exec_num, param_string=None):
 
     if not os.path.exists(root_logdir_path):
         os.makedirs(root_logdir_path)
-
+    print("WORKS?")
     global exec_logdir
     if params:
         exec_logdir = root_logdir_path + '/' + params
@@ -44,7 +45,7 @@ def register(hdfs_exec_dir, endpoint_dir, exec_num, param_string=None):
     global tb_pid
     if tb_pid == 0:
         pypath = os.getenv("PYSPARK_PYTHON")
-
+        print("GOO")
         #find free port
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('',0))
@@ -56,12 +57,14 @@ def register(hdfs_exec_dir, endpoint_dir, exec_num, param_string=None):
         tb_pid = tb_proc.pid
 
         host = socket.gethostname()
+        global tb_url
         tb_url = "http://{0}:{1}".format(host, port)
         global endpoint
         endpoint = endpoint_dir + "/tensorboard.exec" + str(exec_num)
-
+        print("YOYOYO")
         #dump tb host:port to hdfs
     pydoop.hdfs.dump(tb_url, endpoint, user=hopshdfs.project_user())
+    print("ITS FINE")
     return endpoint, tb_pid
 
 def store():
@@ -78,7 +81,7 @@ def logdir():
     return exec_logdir
 
 def clean():
-    shutil.rmtree(exec_logdir)
+    #shutil.rmtree(exec_logdir)
     global params, exec_logdir, events_logdir
     exec_logdir = None
     events_logdir = None
@@ -123,7 +126,7 @@ def start_visualization(hdfs_root_logdir, app_id):
         s.close()
         tb_path = util.find_tensorboard()
         tb_proc = subprocess.Popen([pypath, tb_path, "--logdir=%s" % logdir, "--port=%d" % port],
-                               env=os.environ, preexec_fn=util.on_parent_exit('SIGTERM'))
+                                   env=os.environ, preexec_fn=util.on_executor_exit('SIGTERM'))
 
         host = socket.gethostname()
         tb_url = "http://{0}:{1}".format(host, port)
