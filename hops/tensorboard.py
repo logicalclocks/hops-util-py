0b1a8ca720d1da2868b296195adf69fd62ff10d3
robzor92 <robin.eric.andersson@gmail.com>
2018-09-25 10:47:37 +0200

    fix

--- hops/tensorboard.py
+++ hops/tensorboard.py
@@ -187,7 +187,7 @@ def visualize(spark_session, hdfs_root_logdir):
 
     host = socket.gethostname()
     tb_url = "http://{0}:{1}".format(host, tb_port)
-    tb_endpoint = hopshdfs.get_experiments_dir() + "/" + app_id + "/TensorBoard.driver"
+    tb_endpoint = hopshdfs.get_experiments_dir() + "/" + app_id + "/TensorBoard.visualize"
     #dump tb host:port to hdfs
     pydoop.hdfs.dump(tb_url, tb_endpoint, user=hopshdfs.project_user())
 
