65b84b982d0d42b44eea44100fb30a48660acab7
Kim Hammar <kimham@kth.se>
2018-09-28 10:38:03 +0200

    update docstrings

--- hops/constants.py
+++ hops/constants.py
@@ -1,6 +1,13 @@
-# Environment Variable Names
+"""
+Utility functions to retrieve information about available services and setting up security for the Hops platform.
+
+These utils facilitates development by hiding complexity for programs interacting with Hops services.
+"""
 
 class ENV_VARIABLES:
+    """
+    Environment variable names (accessible in os.environ)
+    """
     KAFKA_BROKERS_ENV_VAR = "KAFKA_BROKERS"
     ELASTIC_ENDPOINT_ENV_VAR = "ELASTIC_ENDPOINT"
     PWD_ENV_VAR = "PWD"
@@ -11,9 +18,10 @@ class ENV_VARIABLES:
     REST_ENDPOINT_END_VAR = "REST_ENDPOINT"
     TENSORFLOW_VERSION_END_VAR = "TENSORFLOW_VERSION"
 
-# Kafka SSL Properties
-
 class KAFKA_SSL_CONFIG:
+    """
+    Kafka SSL constant strings for configuration
+    """
     SSL_TRUSTSTORE_LOCATION_CONFIG = "ssl.truststore.location"
     SSL_TRUSTSTORE_LOCATION_DOC = "The location of the trust store file. "
     SSL_TRUSTSTORE_PASSWORD_CONFIG = "ssl.truststore.password"
@@ -26,6 +34,9 @@ class KAFKA_SSL_CONFIG:
 # General SSL config properties
 
 class SSL_CONFIG:
+    """
+    General SSL configuration constants for Hops-TLS
+    """
     K_CERTIFICATE_CONFIG = "k_certificate"
     T_CERTIFICATE_CONFIG = "t_certificate"
     PEM_K_CERTIFICATE_CONFIG = "k_certificate.pem"
@@ -33,16 +44,18 @@ class SSL_CONFIG:
     DOMAIN_CA_TRUSTSTORE = "domain_ca_truststore"
     CRYPTO_MATERIAL_PASSWORD = "material_passwd"
 
-# Kafka Producer Properties
-
 class KAFKA_PRODUCER_CONFIG:
+    """
+    Constant strings for Kafka producers
+    """
     BOOTSTRAP_SERVERS_CONFIG = "bootstrap.servers"
     KEY_SERIALIZER_CLASS_CONFIG = "key.serializer"
     VALUE_SERIALIZER_CLASS_CONFIG = "value.serializer"
 
-# Kafka Consumer Properties
-
 class KAFKA_CONSUMER_CONFIG:
+    """
+    Constant strings for Kafka consumers
+    """
     GROUP_ID_CONFIG = "group.id"
     ENABLE_AUTO_COMMIT_CONFIG = "enable.auto.commit"
     AUTO_COMMIT_INTERVAL_MS_CONFIG = "auto.commit.interval.ms"
@@ -54,9 +67,10 @@ class KAFKA_CONSUMER_CONFIG:
     KEY_DESERIALIZER_CLASS_CONFIG = "key.deserializer"
     VALUE_DESERIALIZER_CLASS_CONFIG = "value.deserializer"
 
-# JSON properties sent to Hopsworks REST API
-
 class REST_CONFIG:
+    """
+    REST endpoints and JSON properties used for communicating with Hopsworks REST API
+    """
     JSON_JOBSTATE = "running"
     JSON_JOBIDS = "jobIds"
     JSON_KEYSTOREPWD = "keyStorePwd"
