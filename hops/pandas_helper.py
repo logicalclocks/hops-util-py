379a581735e11cf757ff6186d8bbf27d47b9098e
Robin Andersson <robin.eric.andersson@gmail.com>
2019-09-25 10:29:42 +0200

    [HOPSWORKS-1290] numpy_helper and pandas_helper documentation not rendering

--- hops/pandas_helper.py
+++ hops/pandas_helper.py
@@ -1,22 +1,22 @@
 """
-API for opening csv files into Pandas from HopsFS.
+API for opening csv files into Pandas from HDFS
 """
 import hops.hdfs as hdfs
 import pandas as pd
 
 def read_csv(hdfs_filename, **kwds):
     """
-      Reads a comma-separated values (csv) file from HopsFS into a Pandas DataFrame.
+      Reads a comma-separated values (csv) file from HDFS into a Pandas DataFrame
 
       Args:
-         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS).
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
          :**kwds: You can add any additional args found in pandas.read_csv(...) 
 
       Returns:
         A pandas dataframe
 
       Raises:
-        IOError: If the file does not exist.
+        IOError: If the file does not exist
     """
     hdfs_path = hdfs._expand_path(hdfs_filename)    
     h = hdfs.get_fs()
@@ -26,15 +26,15 @@ def read_csv(hdfs_filename, **kwds):
 
 def write_csv(hdfs_filename, dataframe, **kwds):
     """
-      Writes a pandas dataframe to a comma-separated values (csv) text file in HopsFS. Overwrites the file if it already exists.
+      Writes a pandas dataframe to a comma-separated values (csv) text file in HDFS. Overwrites the file if it already exists
 
       Args:
-         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS).
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
          :dataframe: a Pandas dataframe
          :**kwds: You can add any additional args found in pandas.read_csv(...) 
 
       Raises:
-        IOError: If the file does not exist.
+        IOError: If the file does not exist
     """
     hdfs_path = hdfs._expand_path(hdfs_filename, exists=False)    
     h = hdfs.get_fs()
