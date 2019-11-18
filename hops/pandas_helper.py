7ae489ce918732f3dd23608e8d268a3686664cab
Robin Andersson <robin.eric.andersson@gmail.com>
2019-11-18 14:14:44 +0100

    [HOPSWORKS-1391] Extend Pandas and Numpy Helper Classes

--- hops/pandas_helper.py
+++ hops/pandas_helper.py
@@ -24,6 +24,69 @@ def read_csv(hdfs_filename, **kwds):
       data = pd.read_csv(f, **kwds)
     return data
 
+def read_parquet(hdfs_filename, **kwds):
+    """
+      Load a parquet object from a HDFS path, returning a DataFrame.
+
+      Args:
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
+         :**kwds: You can add any additional args found in pandas.read_csv(...) 
+
+      Returns:
+        A pandas dataframe
+
+      Raises:
+        IOError: If the file does not exist
+    """
+    hdfs_path = hdfs._expand_path(hdfs_filename)    
+    h = hdfs.get_fs()
+    with h.open_file(hdfs_path, "rt") as f:
+      data = pd.read_parquet(f, **kwds)
+    return data
+
+def read_json(hdfs_filename, **kwds):
+    """
+      Convert a JSON string to pandas object.
+
+      Args:
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
+         :**kwds: You can add any additional args found in pandas.read_csv(...) 
+
+      Returns:
+        A pandas dataframe
+
+      Raises:
+        IOError: If the file does not exist
+    """
+    hdfs_path = hdfs._expand_path(hdfs_filename)    
+    h = hdfs.get_fs()
+    with h.open_file(hdfs_path, "rt") as f:
+      data = pd.read_json(f, **kwds)
+    return data
+
+def read_excel(hdfs_filename, **kwds):
+    """
+      Retrieve pandas object stored in HDFS file, optionally based on where criteria
+
+
+
+      Args:
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
+         :**kwds: You can add any additional args found in pandas.read_csv(...) 
+
+      Returns:
+        A pandas dataframe
+
+      Raises:
+        IOError: If the file does not exist
+    """
+    hdfs_path = hdfs._expand_path(hdfs_filename)    
+    h = hdfs.get_fs()
+    with h.open_file(hdfs_path, "rt") as f:
+      data = pd.read_excel(f, **kwds)
+    return data
+
+
 def write_csv(hdfs_filename, dataframe, **kwds):
     """
       Writes a pandas dataframe to a comma-separated values (csv) text file in HDFS. Overwrites the file if it already exists
@@ -31,7 +94,7 @@ def write_csv(hdfs_filename, dataframe, **kwds):
       Args:
          :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
          :dataframe: a Pandas dataframe
-         :**kwds: You can add any additional args found in pandas.read_csv(...) 
+         :**kwds: You can add any additional args found in pandas.to_csv(...) 
 
       Raises:
         IOError: If the file does not exist
@@ -42,3 +105,39 @@ def write_csv(hdfs_filename, dataframe, **kwds):
       dataframe.to_csv(f, **kwds)
 
 
+def write_parquet(hdfs_filename, dataframe, **kwds):
+    """
+      Writes a pandas dataframe to a parquet file in HDFS. Overwrites the file if it already exists
+
+      Args:
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
+         :dataframe: a Pandas dataframe
+         :**kwds: You can add any additional args found in pandas.to_parequet(...) 
+
+      Raises:
+        IOError: If the file does not exist
+    """
+    hdfs_path = hdfs._expand_path(hdfs_filename, exists=False)    
+    h = hdfs.get_fs()
+    with h.open_file(hdfs_path, "wb") as f:
+      dataframe.to_parquet(f, **kwds)
+
+
+def write_json(hdfs_filename, dataframe, **kwds):
+    """
+      Writes a pandas dataframe to a JSON file in HDFS. Overwrites the file if it already exists
+
+      Args:
+         :hdfs_filename: You can specify either a full hdfs pathname or a relative one (relative to your Project's path in HDFS)
+         :dataframe: a Pandas dataframe
+         :**kwds: You can add any additional args found in pandas.to_json(...) 
+
+      Raises:
+        IOError: If the file does not exist
+    """
+    hdfs_path = hdfs._expand_path(hdfs_filename, exists=False)    
+    h = hdfs.get_fs()
+    with h.open_file(hdfs_path, "wt") as f:
+      dataframe.to_json(f, **kwds)
+
+      
