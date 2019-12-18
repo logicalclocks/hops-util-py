7804a0d6734fe6e8a23c2598547316d40776e94c
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2019-12-18 18:26:29 +0100

    [HOPSWORKS-1356] Jobs - parallel executions (#134)

--- hops/jobs.py
+++ hops/jobs.py
@@ -6,7 +6,6 @@ Utility functions to manage jobs in Hopsworks.
 from hops import constants, util, hdfs
 from hops.exceptions import RestAPIError
 import json
-import sys
 
 
 def create_job(name, job_config):
@@ -40,9 +39,36 @@ def create_job(name, job_config):
     return response_object
 
 
-def _job_execution_action(name, action):
+def _job_execution_action(name, args=None):
     """
     Manages execution for the given job, start or stop. Submits an http request to the HOPSWORKS REST API.
+
+    Returns:
+        The job status.
+    """
+    method = constants.HTTP_CONFIG.HTTP_PUT
+    resource_url = constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_JOBS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   name + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE
+
+    response = util.send_request(method, resource_url, args)
+    response_object = response.json()
+    if response.status_code >= 400:
+        error_code, error_msg, user_msg = util._parse_rest_error(response_object)
+        raise RestAPIError("Could not perform action on job's execution (url: {}), server response: \n "
+                           "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
+            resource_url, response.status_code, response.reason, error_code, error_msg, user_msg))
+
+    return response_object
+
+
+def start_job(name, args=None):
+    """
+    Start an execution of the job. Only one execution can be active for a job.
 
     Returns:
         The job status.
@@ -54,8 +80,9 @@ def _job_execution_action(name, action):
                    hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
                    constants.REST_CONFIG.HOPSWORKS_JOBS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
                    name + constants.DELIMITERS.SLASH_DELIMITER + \
-                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE + "?action=" + action
-    response = util.send_request(method, resource_url)
+                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE
+
+    response = util.send_request(method, resource_url, args)
     response_object = response.json()
     if response.status_code >= 400:
         error_code, error_msg, user_msg = util._parse_rest_error(response_object)
@@ -66,26 +93,40 @@ def _job_execution_action(name, action):
     return response_object
 
 
-def start_job(name, action="start"):
-    """
-    Start an execution of the job. Only one execution can be active for a job.
-
-    Returns:
-        The job status.
-    """
-    return _job_execution_action(name, action)
-
-
-def stop_job(name, action="stop"):
+def stop_job(name):
     """
     Stop the current execution of the job.
     Returns:
         The job status.
     """
-    return _job_execution_action(name, action)
+    method = constants.HTTP_CONFIG.HTTP_PUT
+    resource_url = constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_JOBS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   name + constants.DELIMITERS.SLASH_DELIMITER + \
+                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
+                   "status"
+
+    status = {"status":"stopped"}
+    response = util.send_request(method, resource_url, data=status)
+    response_object = response.json()
+    if response.status_code >= 400:
+        error_code, error_msg, user_msg = util._parse_rest_error(response_object)
+        raise RestAPIError("Could not perform action on job's execution (url: {}), server response: \n "
+                           "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
+            resource_url, response.status_code, response.reason, error_code, error_msg, user_msg))
+
+    return response_object
 
 
-def get_current_execution(name):
+def get_executions(name, query=None):
+    """
+    Get a list of the currently running executions for this job.
+    Returns:
+        The job status.
+    """
     method = constants.HTTP_CONFIG.HTTP_GET
     resource_url = constants.DELIMITERS.SLASH_DELIMITER + \
                    constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
@@ -93,12 +134,14 @@ def get_current_execution(name):
                    hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
                    constants.REST_CONFIG.HOPSWORKS_JOBS_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
                    name + constants.DELIMITERS.SLASH_DELIMITER + \
-                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE + "?offset=0&limit=1&sort_by=id:desc"
+                   constants.REST_CONFIG.HOPSWORKS_EXECUTIONS_RESOURCE + query
     response = util.send_request(method, resource_url)
     response_object = response.json()
-    if response.status_code >= 400:
+    if response.status_code >= 500:
         error_code, error_msg, user_msg = util._parse_rest_error(response_object)
         raise RestAPIError("Could not get current job's execution (url: {}), server response: \n "
                            "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
             resource_url, response.status_code, response.reason, error_code, error_msg, user_msg))
+    if response.status_code >= 400:
+        return None
     return response_object
