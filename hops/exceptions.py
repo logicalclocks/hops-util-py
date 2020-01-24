1e4f0d1d8fba2cd84587d3ee2d9b14588592a448
Gautier Berthou <gautier@logicalclocks.com>
2020-01-24 17:02:39 +0100

    [HOPSWORKS-1538] add a function to automatically set up the hopsfs and hive clients on a databricks cluster (#149)

--- hops/exceptions.py
+++ hops/exceptions.py
@@ -4,3 +4,6 @@ Common Exceptions thrown by the hops library
 
 class RestAPIError(Exception):
     """This exception will be raised if there is an error response from a REST API call to Hopsworks"""
+
+class UnkownSecretStorageError(Exception):
+    """This exception will be raised if an unused secrets storage is passed as a parameter"""
