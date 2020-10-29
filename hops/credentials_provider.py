5a95c42dad51ff10bf354daff321843fe3f3b48f
Ermias <ermiasg@kth.se>
2020-10-29 15:12:16 +0100

    fix for default

--- hops/credentials_provider.py
+++ hops/credentials_provider.py
@@ -5,36 +5,27 @@ AWS temporary credential provider.
 
 from hops import constants, util, hdfs
 from hops.exceptions import RestAPIError
-import uuid
 import os
 
 
-def assume_role(role, role_session_name=" ", duration_seconds=3600):
+def assume_role(role_arn=None, role_session_name=None, duration_seconds=3600):
     """
     Assume a role and sets the temporary credential to the spark context hadoop configuration and environment variables.
 
     Args:
-        :role: (string) the role arn to be assumed
+        :role_arn: (string) the role arn to be assumed
         :role_session_name: (string) use to uniquely identify a session when the same role is assumed by different principals or for different reasons.
         :duration_seconds: (int) the duration of the session. Maximum session duration is 3600 seconds.
 
     >>> from hops.credentials_provider import assume_role
-    >>> assume_role("arn:aws:iam::<AccountNumber>:role/analyst")
+    >>> assume_role(role_arn="arn:aws:iam::<AccountNumber>:role/analyst")
 
     Returns:
         temporary credentials
     """
-    query = "?" + constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_ROLE + "=" + role
-    query += "&" + constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_SESSION + "="
-    query += role_session_name if role_session_name != " " else "session_" + uuid.uuid4().__str__()
-    if duration_seconds != 3600:
-        query += "&" + constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_SESSION_DURATION + "=" \
-                + duration_seconds
+    query = _query_string(role_arn, role_session_name, duration_seconds)
     method = constants.HTTP_CONFIG.HTTP_GET
-    resource_url = constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + \
-        constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_CLOUD_RESOURCE + \
-        constants.DELIMITERS.SLASH_DELIMITER + \
-        constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE + query
+    resource_url = _get_cloud_resource() + constants.REST_CONFIG.HOPSWORKS_AWS_CLOUD_SESSION_TOKEN_RESOURCE + query
 
     response = util.send_request(method, resource_url)
     json_content = _parse_response(response, resource_url)
@@ -61,12 +52,12 @@ def get_roles():
     return roles
 
 
-def get_role(role_id):
+def get_role(role_id="default"):
     """
-    Get a role arn mapped to the current project by id
+    Get a role arn mapped to the current project by id or if no id is supplied the default role is returned
 
     Args:
-        :role_id: (int) id of the role
+        :role_id: id of the role default
 
     >>> from hops.credentials_provider import get_role
     >>> get_role(id)
@@ -105,11 +96,7 @@ def _get_roles(role_id=None):
     if role_id:
         by_id = constants.DELIMITERS.SLASH_DELIMITER + str(role_id)
     method = constants.HTTP_CONFIG.HTTP_GET
-    resource_url = constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + \
-        constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + \
-        constants.DELIMITERS.SLASH_DELIMITER + hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
-        constants.REST_CONFIG.HOPSWORKS_CLOUD_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER + \
-        constants.REST_CONFIG.HOPSWORKS_CLOUD_ROLE_MAPPINGS_RESOURCE + by_id
+    resource_url = _get_cloud_resource() + constants.REST_CONFIG.HOPSWORKS_CLOUD_ROLE_MAPPINGS_RESOURCE + by_id
     response = util.send_request(method, resource_url)
     return _parse_response(response, resource_url)
 
@@ -122,3 +109,29 @@ def _parse_response(response, url):
         raise RestAPIError("Error calling {}. Got status: HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}"
                            ", user msg: {}".format(url, response.status_code, response.reason, error_code, error_msg,
                                                    user_msg))
+
+
+def _query_string(role, role_session_name, duration_seconds):
+    query = ""
+    if role:
+        query = constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_ROLE + "=" + role
+    if role_session_name:
+        if query != "":
+            query += "&"
+        query += constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_SESSION + "=" + role_session_name
+    if duration_seconds != 3600 and duration_seconds > 0:
+        if query != "":
+            query += "&"
+        query += constants.REST_CONFIG.HOPSWORKS_CLOUD_SESSION_TOKEN_RESOURCE_QUERY_SESSION_DURATION + "=" + \
+                 duration_seconds
+
+    if query != "":
+        query += "?"
+    return query
+
+
+def _get_cloud_resource():
+    return constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + \
+        constants.DELIMITERS.SLASH_DELIMITER + constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + \
+        constants.DELIMITERS.SLASH_DELIMITER + hdfs.project_id() + constants.DELIMITERS.SLASH_DELIMITER + \
+        constants.REST_CONFIG.HOPSWORKS_CLOUD_RESOURCE + constants.DELIMITERS.SLASH_DELIMITER
