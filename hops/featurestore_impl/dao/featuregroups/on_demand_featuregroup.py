c98377796c2c41831c5ea8739738df8c75af8ede
Fabio Buso <fabio@logicalclocks.com>
2020-11-12 23:04:55 +0100

    [HOPSWORKS-2130] Refactor on demand feature groups (#212)

--- hops/featurestore_impl/dao/featuregroups/on_demand_featuregroup.py
+++ hops/featurestore_impl/dao/featuregroups/on_demand_featuregroup.py
@@ -12,6 +12,6 @@ class OnDemandFeaturegroup():
         Args:
             :on_demand_featuregroup_json: JSON representation of the featuregroup, returned from Hopsworks REST API
         """
-        self.jdbc_connector_id = on_demand_featuregroup_json[constants.REST_CONFIG.JSON_FEATUREGROUP_JDBC_CONNECTOR_ID]
-        self.jdbc_connector_name = on_demand_featuregroup_json[constants.REST_CONFIG.JSON_FEATUREGROUP_JDBC_CONNECTOR_NAME]
+        self.jdbc_connector_id = on_demand_featuregroup_json['storageConnector']['id']
+        self.jdbc_connector_name = on_demand_featuregroup_json['storageConnector']['name']
         self.query = on_demand_featuregroup_json[constants.REST_CONFIG.JSON_FEATUREGROUP_ON_DEMAND_QUERY]
