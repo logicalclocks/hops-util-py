d555f6b6f39513f0574c7b8dfe1da0d3c7407bd7
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-12-04 16:15:03 +0100

    [HOPSWORKS-2118] Add download method to hops-util-py (#213)

--- hops/dataset.py
+++ hops/dataset.py
@@ -98,7 +98,7 @@ class HTTPUpload:
                                "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
                                 self.resource, response.status_code, response.reason, error_code, error_msg, user_msg))
 
-        progress = round(self.params['flowChunkNumber'] / self.params['flowTotalChunks'], 3) * 100
+        progress = round(self.params['flowChunkNumber'] / self.params['flowTotalChunks'] * 100, 3)
         print("Progress: " + str(progress) + "%")
         self.params['flowChunkNumber'] += 1
 
@@ -117,6 +117,59 @@ class HTTPUpload:
                 self._next()
 
 
+class HTTPDownload:
+    # stream=True and chunk_size=None will read data as it arrives in whatever size the chunks are received
+    DEFAULT_DOWNLOAD_CHUNK_SIZE = None
+
+    def __init__(self, path, file, chunk_size=DEFAULT_DOWNLOAD_CHUNK_SIZE):
+        self.resource = constants.REST_CONFIG.HOPSWORKS_REST_RESOURCE + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        constants.REST_CONFIG.HOPSWORKS_PROJECT_RESOURCE + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        os.environ[constants.ENV_VARIABLES.HOPSWORKS_PROJECT_ID_ENV_VAR] + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        constants.REST_CONFIG.HOPSWORKS_DATASETS_RESOURCE + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        "download" + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        "with_auth" + \
+                        constants.DELIMITERS.SLASH_DELIMITER + \
+                        path + \
+                        constants.DELIMITERS.QUESTION_MARK_DELIMITER + \
+                        "type=DATASET"
+
+        self.file = file
+        if not chunk_size:
+            self.chunk_size = self.DEFAULT_DOWNLOAD_CHUNK_SIZE
+        else:
+            self.chunk_size = chunk_size
+
+    def download(self):
+        with util.send_request(constants.HTTP_CONFIG.HTTP_GET,
+                               resource="/" + self.resource,
+                               stream=True) as response:
+
+            if response.status_code // 100 != 2:
+                error_code, error_msg, user_msg = util._parse_rest_error(response.json())
+                raise RestAPIError("Could not perform action on job's execution (url: {}), server response: \n "
+                                   "HTTP code: {}, HTTP reason: {}, error code: {}, error msg: {}, user msg: {}".format(
+                                    self.resource, response.status_code, response.reason, error_code, error_msg, user_msg))
+
+            with open(self.file, "wb") as f:
+                downloaded = 0
+                file_size = response.headers.get('Content-Length')
+                if not file_size:
+                    print("Downloading file ...", end=" ")
+                for chunk in response.iter_content(chunk_size=self.chunk_size):
+                    f.write(chunk)
+                    downloaded += len(chunk)
+                    if file_size:
+                        progress = round(downloaded / int(file_size) * 100, 3)
+                        print("Progress: " + str(progress) + "%")
+                if not file_size:
+                    print("DONE")
+    
+
 def upload(file, remote_path, chunk_size=None):
     """
     Upload data to a project's dataset by setting the path to the local file to be uploaded to a remote_path in a
@@ -132,8 +185,32 @@ def upload(file, remote_path, chunk_size=None):
     Args:
         :file: the absolute path or the filename of the file to be uploaded.
         :remote_path: the dataset or the path to the folder in the dataset to upload the file.
+        :chunk_size: (Optional) chunk size used to upload the file
 
     Returns:
         None
     """
     HTTPUpload(file, remote_path, chunk_size).upload()
+
+
+def download(remote_path, file, chunk_size=None):
+    """
+    Download a file from a project's dataset by specifying the path to the remote file and a local path where the
+    file is downloaded. The remote file path must include the project name as shown in the example. It is possible
+    to download files from shared projects by setting the shared project name. If the chunk size is specified, the remote
+    file is downloaded in chunks of similar size. Otherwise, chunks can have any size and are read as they are received.
+
+    Example usage:
+
+    >>> from hops import dataset
+    >>> dataset.download("Projects/project_name/Resources/myremotefile.txt", "tmp/mylocalfile.txt")
+
+    Args:
+        :remote_path: the path to the remote file in the dataset
+        :file: the absolute or relative path where the file is downloaded
+        :chunk_size: (Optional) chunk size used to download the file
+
+    Returns:
+        None
+    """
+    HTTPDownload(remote_path, file, chunk_size).download()
