d1beb17fc680948d12d46934b226aea16f29068f
moritzmeister <meister.mo@gmail.com>
2021-01-22 10:43:55 +0100

    deprecate featurestore module

--- hops/featurestore_impl/dao/common/featurestore_entity.py
+++
@@ -1,7 +0,0 @@
-from hops.featurestore_impl.dao.features.feature import Feature
-
-class FeaturestoreEntity(object):
-    """
-    Represents an abstract featurestore entity (contains common functionality between feature groups and
-    training datasets in the featurestore
-    """
